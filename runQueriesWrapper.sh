b44fb0d6429633ec8108f6887aac4408f552d565
Charlie <ncsucharlie@gmail.com>
2018-11-10 17:20:01 -0500

    Fix error in wrapper shell

--- runQueriesWrapper.sh
+++ runQueriesWrapper.sh
@@ -1,4 +1,4 @@
-cd /home/charlie/projects/nba-sdql
+cd /home/charlie/github/ncaabb-sdql
 
 LOGFILE=output.log
-node runQueries.js -m ncsucharlie@gmail.com | tee output.log
+node runQueries.js -m ncsucharlie@gmail.com | tee $LOGFILE
