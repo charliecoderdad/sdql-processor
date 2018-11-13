42ee40bc251e2fd2a084180cdf7c7ad9dcacfc3a
Charlie <ncsucharlie@gmail.com>
2018-11-13 15:21:40 -0500

    Fix wrapper scripts to work with new all-sports sdql tool

---
+++ executionWrappers/runNcaaFootball.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+SDQL_HOME_DIR=/home/charlie/github/sdql-processor
+QUERY_FILE=$SDQL_HOME_DIR/queries/queries-ncaafb-all.txt
+LOGFILE=$SDQL_HOME_DIR/logs/ncaafb-output.log
+EMAIL_ADDY=ncsucharlie@gmail.com
+
+mkdir -p $SDQL_HOME_DIR/logs
+
+echo `date` > $LOGFILE
+
+cd $SDQL_HOME_DIR
+node runQueries.js -f $QUERY_FILE -m $EMAIL_ADDY | tee -a $LOGFILE
--- runQueriesWrapper.sh
+++
@@ -1,4 +0,0 @@
-cd /home/charlie/github/ncaabb-sdql
-
-LOGFILE=output.log
-node runQueries.js -m ncsucharlie@gmail.com | tee $LOGFILE
